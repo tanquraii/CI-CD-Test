659db50948ba0df8b69be5a4751ffdacb0b494eb
tanquraii <higashigatajosuke16@gmail.com>
2025-05-13 11:48:03 +0600

    third commit

--- movies_project/movies_project/settings.py
+++ movies_project/movies_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'catusers',
         'USER':'postgres',
-        'PASSWORD':'SIKE',
+        'PASSWORD':'123',
         'HOST':'localhost',
         'PORT':5432
     }
